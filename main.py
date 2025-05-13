0dfbb3b1f2a3e64e1983120ad5d15729c53c499f
Rohit Anand <rohit.anand@birdeye.com>
2025-05-13 17:09:50 +0530

    html files

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import asyncio
 import dlc
 import reviewus_rate
 from fastapi import FastAPI
+import os
+import glob
 import json
 import time
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    'null'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -23,41 +40,63 @@ async def run_reviewus():
     await reviewus_rate.main()
     return {"status": "Review Us Task Completed"}
 
+@app.get("/getAutomationResult")
+def getAutomationResult():
+    directory = "outputs"
+    files = glob.glob(f"{directory}/result_*.json")
+    if not files:
+        return {"error": "No result files found"}
+    
+    latest_file = max(files, key=os.path.getctime)
+    with open(latest_file, "r") as file:
+        data = json.load(file)
+
+    return {"final_result": data}
+
 @app.get("/testpublicformulrs")
 async def testpublicformulrs():
     start_timestamp = time.time()
-    # print("Running All Tasks...")
-    # print("Running DLC Task...")
-    # dlc_history = await dlc.main()
-    # print("\nDLC Task Completed.\n")
-    # print("------------------------\n")
-    # print("Running Review Us Task...")
-    # reviewusrate_history = await reviewus_rate.main()
-    # print("\nReview Us Task Completed.\n")
-    # print("------------------------\n")
-    # print("All Tasks Completed.\n")
-    # final_result = {
-    #     "DLC Page Success": dlc_history.is_successful(), 
-    #     "DLC Page Final Result": dlc_history.final_result(),
-    #     "Review Us Page Success": reviewusrate_history.is_successful(),
-    #     "Review Us Page Final Result": reviewusrate_history.final_result()
-    # }
+    print("Running All Tasks...")
+    print("Running DLC Task...")
+    dlc_history = await dlc.main()
+    print("\nDLC Task Completed.\n")
+    print("------------------------\n")
+    print("Running Review Us Task...")
+    reviewusrate_history = await reviewus_rate.main()
+    print("\nReview Us Task Completed.\n")
+    print("------------------------\n")
+    print("All Tasks Completed.\n")
 
     end_timestamp = time.time()
     final_result = {
         "start":start_timestamp,
         "end": end_timestamp,
         "pages": {
-            "DLC Page": {
-                "success": True,
-                "message": "DLC Page is working as expected",
+            "DLC": {
+                "success": dlc_history.is_successful(),
+                "message": dlc_history.final_result(),
             }, 
-            "Review us Page": {
-                "success": True,
-                "message": "Review Us Page is working as expected",
+            "Review us": {
+                "success": reviewusrate_history.is_successful(),
+                "message": reviewusrate_history.final_result()
             },
         }
     }
+
+    # final_result = {
+    #     "start":start_timestamp,
+    #     "end": end_timestamp,
+    #     "pages": {
+    #         "DLC Page": {
+    #             "success": True,
+    #             "message": "DLC Page is working as expected/....",
+    #         }, 
+    #         "Review us Page": {
+    #             "success": True,
+    #             "message": "Review Us Page is working as expected/...",
+    #         },
+    #     }
+    # }
     final_result_obj = json.dumps(final_result, indent=4)
     
     print(f"Timestamp: {end_timestamp}")
